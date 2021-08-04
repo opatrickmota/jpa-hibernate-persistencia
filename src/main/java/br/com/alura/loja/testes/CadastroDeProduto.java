99d8279c9c785ec6255c92f0d879f8c907f46cfb
Patrick Mota <patrickvieiramota@gmail.com>
2021-08-04 19:03:30 -0300

    Consultando as entidades

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -10,12 +10,23 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarTodos();
+        todos.forEach(p2 -> System.out.println(p2.getNome()));
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
-
-
         Produto celular = new Produto("Xiaome Redmi", "Muito legal", new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
@@ -30,6 +41,5 @@ public class CadastroDeProduto {
 
         em.getTransaction().commit();
         em.close();
-
     }
 }
